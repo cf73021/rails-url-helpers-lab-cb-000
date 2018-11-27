2e070adfebdc2644450b0970efba4f159d063f72
Chris Franklin <cf73021@gmail.com>
2018-11-27 20:43:07 +0000

    automatically backed up by learn

--- db/migrate/002_create_active_field.rb
+++ db/migrate/002_create_active_field.rb
@@ -1,8 +1,6 @@
 class CreateActiveField < ActiveRecord::Migration
   def change
-    add_column :students do |t|
-      t.string :first_name
-      t.string :last_name
+    add_column :students, :active, :boolean
 
       t.timestamps null: false
     end
