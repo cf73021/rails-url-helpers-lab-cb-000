ae219f499d4002ebdc080d4339f53023032a24e5
Chris Franklin <cf73021@gmail.com>
2018-11-27 21:40:41 +0000

    automatically backed up by learn

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -8,6 +8,11 @@ class StudentsController < ApplicationController
   def show
   end
 
+  def activate
+    @student = Student.find(params[:id])
+    @student.set_status
+    redirect_to @student
+
   private
 
     def set_student
